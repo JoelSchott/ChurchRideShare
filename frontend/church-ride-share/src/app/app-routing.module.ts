cb7d9a57b6dde4a801693ab49227b1ab5da98e15
Christopher Spencer <cmspencer109@gmail.com>
2023-03-14 12:22:14 -0500

    Create sign up choice page

--- frontend/church-ride-share/src/app/app-routing.module.ts
+++ frontend/church-ride-share/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './features/home-page/home-page.component';
+import { SignUpChoiceComponent } from './features/sign-up-choice/sign-up-choice.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'signup', component: SignUpChoiceComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/church-ride-share/src/app/features/sign-up-choice/sign-up-choice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-choice',
+  templateUrl: './sign-up-choice.component.html',
+  styleUrls: ['./sign-up-choice.component.scss']
+})
+export class SignUpChoiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
