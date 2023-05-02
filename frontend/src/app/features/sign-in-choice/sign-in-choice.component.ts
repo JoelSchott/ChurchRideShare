01d1c0070c1231f2ce34b8fcc2b821c6804dd7e1
Garrett Mason <gmmncf@mst.edu>
2023-05-02 16:22:59 -0500

    Fixed sign in token retrieval

--- frontend/src/app/features/sign-in-choice/sign-in-choice.component.ts
+++ frontend/src/app/features/sign-in-choice/sign-in-choice.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalConstants } from 'src/app/global';
-import { ActivatedRoute } from '@angular/router';
-import { JwtTokenService } from 'src/app/services/jwt-token.service';
-import { CookieService } from 'ngx-cookie-service';
-
 
 @Component({
   selector: 'sign-in-choice',
@@ -15,26 +11,8 @@ export class SignInChoiceComponent implements OnInit {
   public driverUrl: string= GlobalConstants.driverSignInCognitoUrl;
   public churchUrl: string= GlobalConstants.churchSignInCognitoUrl;
 
-  constructor(private route: ActivatedRoute, 
-              private jwtService: JwtTokenService,
-              private cookieService: CookieService,) { }
+  constructor() { }
 
-  ngOnInit(): void {
-    this.checkForUrlToken();
-  }
-  
-  checkForUrlToken(){
-    this.route.fragment.subscribe((fragment) => {
-      if(fragment){
-        // Saving token in cookies allows to retrieve token even after page refresh
-        this.cookieService.set("driverToken", fragment);
-      }
-    })
-
-    // Works after page refresh (if given token in url)
-    this.jwtService.setToken(this.cookieService.get("driverToken"));
-    // Prints token
-    if(this.cookieService.get("driverToken")) console.log(this.jwtService.getDecodeToken());
-  }
+  ngOnInit(): void {  }
 
 }
--- frontend/src/app/features/sign-up-choice/sign-up-choice.component.ts
+++ frontend/src/app/features/sign-up-choice/sign-up-choice.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { GlobalConstants } from 'src/app/global';
+import { JwtTokenService } from 'src/app/services/jwt-token.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'sign-up-choice',
@@ -11,9 +14,26 @@ export class SignUpChoiceComponent implements OnInit {
   public driverUrl: string= GlobalConstants.driverSignUpCognitoUrl;
   public churchUrl: string= GlobalConstants.churchSignUpCognitoUrl;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, 
+              private jwtService: JwtTokenService,
+              private cookieService: CookieService,) { }
 
   ngOnInit(): void {
+    this.checkForUrlToken();
+  }
+
+  checkForUrlToken(){
+    this.route.fragment.subscribe((fragment) => {
+      if(fragment){
+        // Saving token in cookies allows to retrieve token even after page refresh
+        this.cookieService.set("driverToken", fragment);
+      }
+    })
+
+    // Works after page refresh (if given token in url)
+    this.jwtService.setToken(this.cookieService.get("driverToken"));
+    // Prints token
+    if(this.cookieService.get("driverToken")) console.log(this.jwtService.getDecodeToken());
   }
 
 }
